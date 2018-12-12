027b002d8a37024051c8cfa1f32179bfcd730420
Emmanuel <45578601+huw111student@users.noreply.github.com>
2018-12-12 18:22:30 -0500

    Add files via upload

---
+++ onlineLibrary/inc/data.php
@@ -0,0 +1,175 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Developing leader Within You",
+	"img" => "img/media/developingtheleaderwithinyou.jpg",
+    "genre" => "Tech",
+    "format" => "Paperback",
+    "year" => 1993,
+    "category" => "Books",
+    "authors" => [
+        "John Maxwell",
+
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '978-0201633610'
+];
+$catalog[102] = [
+    "title" => "Outwitting The Devil",
+    "img" => "img/media/Outwittingthedevil.jpg",
+    "genre" => "Tech",
+    "format" => "Ebook",
+    "year" => 2011,
+    "category" => "Books",
+    "authors" => [
+        "Napolen Hill"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '978-0132350884'
+];
+$catalog[103] = [
+    "title" => "Rich Dad Poor Dad",
+    "img" => "img/media/richdadpoordad.jpg",
+    "genre" => "Tech",
+    "format" => "Hardcover",
+    "year" => 1997,
+    "category" => "Books",
+    "authors" => [
+        "Robert Kiyoaski",
+
+    ],
+    "publisher" => "Addison-Wesley Professional",
+    "isbn" => '978-0201485677'
+];
+$catalog[104] = [
+    "title" => "Think and Grow Rich",
+    "img" => "img/media/thinkandgrowrich.jpg",
+    "genre" => "Tech",
+    "format" => "Audio",
+    "year" => 1937,
+    "category" => "Books",
+    "authors" => [
+        "Napolen Hill"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '007-6092046981'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Avenger Infinty War",
+    "img" => "img/media/avengerinfintywar.jpeg",
+    "genre" => "Drama",
+    "format" => "DVD",
+    "year" => 2018,
+    "category" => "Movies",
+    "director" => "Robert Zemeckis",
+    "writers" => [
+        "christopher markus",
+        "stephen mcfreely"
+    ],
+    "stars" => [
+        "robert downey jr",
+        "tom holland",
+        "chris hemsworth",
+        "chris pratt"
+    ]
+];
+$catalog[202] = [
+    "title" => "the dark knight",
+    "img" => "img/media/Thedarkknight.jpg",
+    "genre" => "Comedy",
+    "format" => "Blu-ray",
+    "year" => 2008,
+    "category" => "Movies",
+    "director" => "christopher nolan",
+    "writers" => [
+        "William Goldman"
+    ],
+    "stars" => [
+        "christian bale",
+        "heath ledger",
+        "gary oldman",
+        "aaron",
+
+    ]
+];
+$catalog[203] = [
+    "title" => "spiderman homecoming",
+    "img" => "img/media/spidermanhomecoming.png",
+    "genre" => "Drama",
+    "format" => "Blu-ray",
+    "year" => 2017,
+    "category" => "Movies",
+    "director" => "jon watts",
+    "writers" => [
+        "J.R.R. Tolkien",
+        "Fran Walsh",
+        "Philippa Boyens",
+        "Peter Jackson"
+    ],
+    "stars" => [
+        "tom holland",
+        "zendaya",
+        "robert downey jr",
+        "michael toomes",
+
+    ]
+];
+$catalog[204] = [
+    "title" => "captain america civil war",
+    "img" => "img/media/captainamericacivilwar.jpg",
+    "genre" => "Comedy",
+    "format" => "DVD",
+    "year" => 2016,
+    "category" => "Movies",
+    "director" => "joe and anthony russo",
+    "writers" => [
+        "christopher markus"
+    ],
+    "stars" => [
+        "robert downey jr",
+        "chris evans",
+        "anthony mackie",
+        "paul bettany",
+
+
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "lil wayne: the carter",
+    "img" => "img/media/lilwaynethecarter.jpg",
+    "genre" => "rap",
+    "format" => "CD",
+    "year" => 2018,
+    "category" => "Music",
+    "artist" => "lil wayne"
+];
+$catalog[302] = [
+    "title" => "eminem",
+    "img" => "img/media/eminemrecovery.jpg",
+    "genre" => "rap",
+    "format" => "Vinyl",
+    "year" => 2010,
+    "category" => "Music",
+    "artist" => "eminem"
+];
+$catalog[303] = [
+    "title" => "j cole",
+    "img" => "img/media/jcoleforesthilldrive.jpg",
+    "genre" => "rap",
+    "format" => "Cassette",
+    "year" => 2014,
+    "category" => "Music",
+    "artist" => "j cole"
+];
+$catalog[304] = [
+    "title" => "justin timberlake exprience",
+    "img" => "img/media/justintimberlakeexperience.jpg",
+    "genre" => "rap",
+    "format" => "MP3",
+    "year" => 2013,
+    "category" => "Music",
+    "artist" => "justin timberlake"
+];
